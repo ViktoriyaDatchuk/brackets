eee72f4fd294385276f36afd9e575d67d65a281f
Viktoriya Datchuk <grelada@tut.by>
2022-07-26 14:08:11 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+  let stek = [];
+  let yes = true;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let brackets of bracketsConfig) {
+      if (array[i] === brackets[0] && array[i] !== brackets[1]) {
+        stek.push(array[i]);
+      } else if (array[i] === brackets[0] && array[i] === brackets[1] && yes === true) {
+          stek.push(array[i]);
+          yes = false;
+        } else if (array[i] === brackets[0] && array[i] === brackets[1] && yes === false && stek[stek.length - 1] === brackets[1]) {
+          yes = true;
+          stek.pop();
+        } else if (array[i] === brackets[1] && stek[stek.length - 1] === brackets[0]) {
+          stek.pop();
+        } else if (array[i] === brackets[1] && !stek.length) return false;
+    }
+  }
+
+  if (!stek.length && yes) return true;
+
+  return false;
 }
